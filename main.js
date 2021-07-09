ae400cd217b4eb5e2852c36ed4e819305d118135
poby123 <youngsm12@naver.com>
2021-07-09 11:17:01 +0900

    fix style

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ categories.forEach((categoryDir) => {
 
   // read
   const categoryReadDir = path.join(contentDir, categoryDir);
-  const t = fs.readdirSync(categoryReadDir);
+  const categoryDirContent = fs.readdirSync(categoryReadDir);
 
   // write to deploy
   const categoryWriteDir = path.join(targetDir, categoryDir);
@@ -39,14 +39,16 @@ categories.forEach((categoryDir) => {
     fs.mkdirSync(categoryWriteDir);
   }
 
-  t.forEach((file) => {
+  categoryDirContent.forEach((file) => {
     const fileReadDir = path.join(categoryReadDir, file);
     const fileWriteDir = path.join(categoryWriteDir, file);
     const curFileContent = fs.readFileSync(fileReadDir, 'utf-8');
+    const fileStat = fs.statSync(fileReadDir);
     const filename = path.parse(file).name;
-    curContent.files.push(file);
-
-    console.log(file);
+    curContent.files.push({
+      file,
+      fileStat,
+    });
 
     let fileEjsContent = ejs.render(articleHtmlFormat, { article_content: curFileContent });
     fileEjsContent = ejs.render(articleWrapperFormat, {
@@ -67,8 +69,23 @@ content.forEach((cur) => {
   const category = cur.category;
   const files = cur.files;
 
-  const lists = files.map((file) => {
-    return `${category}/${file}`;
+  const lists = files.map(({ file, fileStat }) => {
+    const filename = path.parse(file).name;
+    const modifiedTime = fileStat.mtime;
+
+    const mYear = modifiedTime.getFullYear();
+    const mMonth = `0${modifiedTime.getMonth() + 1}`.slice(-2);
+    const mDay = `0${modifiedTime.getDate()}`.slice(-2);
+
+    const mHH = `0${modifiedTime.getHours()}`.slice(-2);
+    const mMM = `0${modifiedTime.getMinutes()}`.slice(-2);
+    const mSS = `0${modifiedTime.getSeconds()}`.slice(-2);
+
+    return {
+      link: `/deploy/${category}/${filename}.html`,
+      name: `${filename}`,
+      modifiedTime: `${mYear}.${mMonth}.${mDay} ${mHH}:${mMM}:${mSS}`,
+    };
   });
 
   let categoryEjsContent = ejs.render(listHtmlFormat, {
@@ -76,17 +93,23 @@ content.forEach((cur) => {
   });
 
   categoryEjsContent = ejs.render(articleWrapperFormat, {
-    title: 'Files',
+    title: category,
     content: categoryEjsContent,
   });
 
   fs.writeFileSync(`${targetDir}/${category}.html`, categoryEjsContent);
 });
 
+console.log(content);
+
 // category lists
 const categoriesEjsList = ejs.render(listHtmlFormat, {
-  lists: categories.map((category) => {
-    return `${category}.html`;
+  lists: content.map((category) => {
+    return {
+      link: `/deploy/${category.category}.html`,
+      name: category.category,
+      num: category.files.length,
+    };
   }),
 });
 
